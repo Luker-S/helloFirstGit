78e167222c69f1497759770315099254cde11ee7
Administrator <1874512935@qq.com>
2020-10-20 10:18:05 +0800

    edit

--- src/helloFirstGit/HelloMyFirstGit.java
+++ src/helloFirstGit/HelloMyFirstGit.java
@@ -4,6 +4,6 @@ public class HelloMyFirstGit {
 	public static void main(String[] args) {
 		String keyWord = "Hello World";
 		String endWord = " !!!";
-		System.out.printf(" 哈哈哈 这是我的第一个git项目 %s%s",keyWord,endWord);
+		System.out.printf(" 哈哈哈哈 这是我的第一个git项目 %s%s",keyWord,endWord);
 	}
 }
