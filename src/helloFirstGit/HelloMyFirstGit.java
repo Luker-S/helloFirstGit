81063fdf6f95ff8b42ad2740510e685666f2829a
Administrator <1874512935@qq.com>
2020-10-20 10:02:36 +0800

    test Luker-S first Git project

---
+++ src/helloFirstGit/HelloMyFirstGit.java
@@ -0,0 +1,9 @@
+package helloFirstGit;
+
+public class HelloMyFirstGit {
+	public static void main(String[] args) {
+		String keyWord = "Hello World";
+		String endWord = " !!!";
+		System.out.printf(" 我的第一个git项目 %s%s",keyWord,endWord);
+	}
+}
